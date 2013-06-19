095367599e08dbfa2b8652454a15b1a2acabff8d
Vladimir Dmitriev <vldmit@gmail.com>
2013-06-19 11:38:12 +0400

    Bumping to version 0.2, public release

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='django-fspages',
-      description='Django application for serving templates from the file system',
+      description='Django application for serving page templates from the file system',
       author='Vladimir Dmitriev',
       author_email='vldmit@gmail.com',
-      version='0.1',
+      version='0.2',
       packages=['fspages', 'fspages.template', 'fspages.template.loaders'],
       license='BSD',
+      long_description=open('README.rst').read(),
       )
